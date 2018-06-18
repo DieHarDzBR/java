5062a3c0ca4671ed471e119cc5be883b4f7a2916
diogo.dssilva <diogo.dssilva@TAT0541208W10-1.senacsp.edu.br>
2018-06-18 13:55:37 -0300

    Atualizando git

---
+++ Fundamentos Do Java/src/br/com/java/CalculoValorDeServico.java
@@ -0,0 +1,21 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class CalculoValorDeServico {
+
+	public static void main(String[] args) {
+
+		double remuneracao, custo, horas, total;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("_____Calculadora do serviço_____");
+		System.out.println("Remuneração Mensal: ");
+		remuneracao = teclado.nextDouble();
+		System.out.println("Custo operacional mensal: ");
+		custo = teclado.nextDouble();
+		System.out.println("Carga horária mensal de trabalho: ");
+		custo = teclado.nextDouble();
+		
+	}
+
+}
