259020319e52f727f733e66845b18952bd24fb6b
diogo.dssilva <diogo.dssilva@TAT0541208W10-1.senacsp.edu.br>
2018-06-20 15:15:11 -0300

    churrasco

---
+++ Fundamentos Do Java/src/br/com/java/ChurrasColadora.java
@@ -0,0 +1,18 @@
+
+/**
+ * Atividade de avaliação - Churrascoladora
+ * @author Richard Smarsi, Diogo Diniz, Luis Freitas
+ */
+
+
+
+package br.com.java;
+
+public class ChurrasColadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
