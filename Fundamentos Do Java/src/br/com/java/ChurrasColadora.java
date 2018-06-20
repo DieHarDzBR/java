5f039faa54df2454ed43d486881b9f559e8bbbfe
diogo.dssilva <diogo.dssilva@TAT0541208W10-1.senacsp.edu.br>
2018-06-20 15:15:21 -0300

    churrascoladora

--- Fundamentos Do Java/src/br/com/java/ChurrasColadora.java
+++ Fundamentos Do Java/src/br/com/java/ChurrasColadora.java
@@ -8,11 +8,36 @@
 
 package br.com.java;
 
+import java.util.Scanner;
+
 public class ChurrasColadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//variáveis
+		
+		Scanner teclado = new Scanner(System.in);
+		//entrada
+		System.out.println("##### Churrascoladora 2000 #####");
+		System.out.println("Quantidade de homens: ");
+		System.out.println("Quantidade de mulheres: ");
+		System.out.println("Quantidade de crianças: ");
+		System.out.println("Preço médio do Kg da carne: ");
+		System.out.println("Preço médio da lata de cerveja: ");
+		System.out.println("Preço médio da lata de refri: ");
+		//processamento
+		
+		
+		//saída
+		System.out.println("");
+		System.out.println("##################");
+		System.out.println("Lista de compras");
+		//exibir quantidade e preço
+		System.out.println("Quantidade total de carne:");
+		System.out.println("Quantidade total de ceveja: ");
+		System.out.println("Quantidade total de refri: ");
 
+		
+		
 	}
 
 }
